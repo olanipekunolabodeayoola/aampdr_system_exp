169c88ce3e1324b1e9abcebd396933d8a96d0561
kolitha warnakulasooriya <warnakulasooriyakolitha@gmail.com>
2025-05-25 15:17:56 -0500

    update main

---
+++ src/main.py
@@ -0,0 +1,30 @@
+import navio.util
+from state import State
+from control import Control
+from sensors import RawSensorData, Sensors
+from pid_controller import PID
+
+class Drone:
+    
+    def __init__(self):
+        
+        navio.util.check_apm()
+        
+        self.state: State = State()
+        self.sensors: Sensors = Sensors()
+        self.control: Control = Control()
+        self.pid: PID = PID()
+        
+        self.sensors.initialize()
+        
+    def execute(self):
+        
+        while True:
+            
+            sensor_data: RawSensorData = self.sensors.read_data()
+            
+            self.state.update_state(sensor_data)
+            self.control.update()
+            
+            self.pid.calculate_set_points(self.state, self.control)
+            
