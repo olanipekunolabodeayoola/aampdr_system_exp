9e7e82088133217ac31aa3e2dbf0161c9b7e1204
kolitha warnakulasooriya <warnakulasooriyakolitha@gmail.com>
2025-05-25 15:17:32 -0500

    update state

--- src/state.py
+++ src/state.py
@@ -1,47 +1,6 @@
+from vector import Vector
+from sensors import RawSensorData
 import math
-class Vector:
-    
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-        
-    def update_x(self, x: float) -> None:
-        self.x += x
-        
-    def update_y(self, y: float) -> None:
-        self.y += y
-    
-    def update_z(self, z: float) -> None:
-        self.y += z
-        
-    def update(self, x: float, y: float, z: float) -> None:
-        self.update_x(x)
-        self.update_y(y)
-        self.update_z(z)
-        
-    def set_x(self, x: float) -> None:
-        self.x = x
-        
-    def set_y(self, y: float) -> None:
-        self.y = y
-    
-    def set_z(self, z: float) -> None:
-        self.y = z
-        
-    def set(self, x: float, y: float, z: float) -> None:
-        self.set_x(x)
-        self.set_y(y)
-        self.set_z(z)
-        
-    def fill_vector(self, v) -> None:
-        self.x = v.x
-        self.y = v.y
-        self.z = v.z
-        
-    def calculate_size(self):
-        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
-
 class State:
     
     def __init__(self):
@@ -56,8 +15,11 @@ class State:
         
         self.pitch = 0.0
         self.yaw = 0.0
-        self.throttle = 0.0
-        
+        self.roll = 0.0
+    
+    def update_state(self, data: RawSensorData) -> None:
+        self.update_state(data.gyro.x, data.gyro.y, data.gyro.z, data.acc.x, data.acc.y, data.acc.z)    
+    
     def update_state(self, gyro_raw_x: float, gyro_raw_y: float, gyro_raw_z: float, acc_raw_x: float, acc_raw_y: float, acc_raw_z: float)->None:
         
         self.gyro_raw.set(gyro_raw_x, gyro_raw_y, gyro_raw_z)
