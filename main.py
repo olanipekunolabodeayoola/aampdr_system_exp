fa7a7a28f94eb34ef13a9cd7923c8e1b3ef40348
Olabode Olanipekun <olanipekunolabodeayoola@gmail.com>
2025-05-28 06:21:31 -0500

    Add code for getting IMU data, precisely displaying accelerometer data, only.

--- main.py
+++ main.py
@@ -11,22 +11,45 @@
 # Import modules and libraries:
 import sys, time
 
-# Adding Navio2/Python path
-sys.path.insert(0, 'Navio2/Python')
+# Adding path
+#sys.path.insert(0, 'Navio2/Python/navio')
+sys.path.append('Navio2/Python/navio')
 
-import navio.rcinput
+import pwm
+
+sys.path.append('Navio2/Python')
+
+import navio.rcinput    # For Rx, Tx
+import navio.mpu9250    # For IMU
 import navio.util
 
-navio.util.check_apm()
+navio.util.check_apm()    # To check status of apm (Is apm alias for ArduPilot?)
 
+# Setup RCIO:
 # Initialize vector, rcin, with object from navio.rcinput parent
 # class:
 rcin = navio.rcinput.RCInput()
 
-while (1):
-    ch0 = rcin.read(0)    # Call accessor fxn, pass address for first element and...
-    ch1 = rcin.read(1)    # initialize variables, accordingly
-    ch2 = rcin.read(2)
-    ch3 = rcin.read(3)
+# Setup IMU:
+imu = navio.mpu9250.MPU9250()    # Initialize IMU object
+imu.initialize()                 # Activate/initialize newly created
+                                 # IMU object
+
+# Next, first channel, 'ch0' initialized by calling accessor
+# fxn and passing index, accordingly:
+while(True):
+    # Step 1: Read RCIO data
+    ch0 = rcin.read(0)    # Corresponds to roll on Tx
+    ch1 = rcin.read(1)    # pitch
+    ch2 = rcin.read(2)    # throtthle
+    ch3 = rcin.read(3)    # yaw
     print(ch0, ch1, ch2, ch3)
+    time.sleep(1)
+    # Step 2: Read IMU data:
+    imu_data = imu.getMotion9()
+    acc_data = imu_data[0]
+    print('')    # newline
+    #print(m9a[0], m9a[1], m9a[2])
+    print(imu_data[0])
+    # print(imu_data[0])
     time.sleep(1)
